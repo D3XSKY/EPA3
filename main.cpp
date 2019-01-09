9c7e9818363e3ab9edffec670ca5b686080d60ab
d3xsky <dmisic@gmail.com>
2019-01-09 23:11:28 +0100

    first

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  EPA3
+//
+//  Created by Dejan Misic on 09/01/2019.
+//  Copyright © 2019 Dejan Misic. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+    
+}
